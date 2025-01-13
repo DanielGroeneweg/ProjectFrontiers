2b4284fad41f416b864c8a69ea02b7b9108bdb25
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-01-13 13:35:35 +0100

    added some files for asset sorting

--- Project Frontiers/Assets/Scripts/DrivingBehaviour.cs
+++ Project Frontiers/Assets/Scripts/DrivingBehaviour.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarDrivingBehaviour : MonoBehaviour
+public class DrivingBehaviour : MonoBehaviour
 {
+    #region InUnityInspector
     [Header("Required Components")]
     [SerializeField] private Rigidbody rb;
 
@@ -12,38 +13,40 @@ public class CarDrivingBehaviour : MonoBehaviour
     [SerializeField] private float maxSpeed;
     [SerializeField] private float maxReverseSpeed;
     [SerializeField] private float rotationSpeed;
+    #endregion
 
-    // Internal
+    #region InternalVariables
+    // Player Input
     private bool pressedW;
     private bool pressedA;
     private bool pressedS;
     private bool pressedD;
 
+    // Physics
     private float velocity;
+    #endregion
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W)) pressedW = true;
-        if (Input.GetKey(KeyCode.A)) pressedA = true;
-        if (Input.GetKey(KeyCode.S)) pressedS = true;
-        if (Input.GetKey(KeyCode.D)) pressedD = true;
+        GetInputBools();
     }
 
     private void FixedUpdate()
     {
         DoAcceleration();
         DoSteering();
-        ResetBools();
+        ResetInputBools();
     }
-
     private void DoAcceleration()
     {
+        // Forward
         if (pressedW && velocity < maxSpeed)
         {
             rb.AddForce(transform.forward * speed);
             velocity += speed;
         }
 
+        // Backward/Breaking
         if (pressedS && velocity > -maxReverseSpeed)
         {
             rb.AddForce(-transform.forward * speed);
@@ -59,7 +62,14 @@ public class CarDrivingBehaviour : MonoBehaviour
         if (pressedA) rb.angularVelocity -= new Vector3(0, 1, 0) * rotationSpeed;
         if (pressedD) rb.angularVelocity += new Vector3(0, 1, 0) * rotationSpeed;
     }
-    private void ResetBools()
+    private void GetInputBools()
+    {
+        if (Input.GetKey(KeyCode.W)) pressedW = true;
+        if (Input.GetKey(KeyCode.A)) pressedA = true;
+        if (Input.GetKey(KeyCode.S)) pressedS = true;
+        if (Input.GetKey(KeyCode.D)) pressedD = true;
+    }
+    private void ResetInputBools()
     {
         pressedW = false;
         pressedA = false;
