9087feb691161c33ffd4a11a4f65f501ca52c881
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-01-14 15:20:25 +0100

    created a scene for myself, created a timer that counts down, created a finishline

---
+++ Project Frontiers/Assets/Scripts/TriggerInvoker.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerInvoker : MonoBehaviour
+{
+    public UnityEvent triggerEnter;
+    public UnityEvent triggerStay;
+    public UnityEvent triggerExit;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        triggerEnter?.Invoke();
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        triggerStay?.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExit?.Invoke();
+    }
+}
