49b8e1ab5f21e3c9f07e638ff7661c998b0e4930
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-01-26 20:20:23 +0100

    changed fmod project location to the asset folder, now it works on every pc

--- Project Frontiers/Assets/Scripts/PlayerScripts/FMOD_EngineSoundScript.cs
+++ Project Frontiers/Assets/Scripts/PlayerScripts/FMOD_EngineSoundScript.cs
@@ -7,13 +7,14 @@ using UnityEngine;
 public class FMOD_EngineSoundScript : MonoBehaviour
 {
     [SerializeField] private CarControl carControl;
-    public StudioEventEmitter emitter;
+    [SerializeField] private StudioEventEmitter emitter;
+    [SerializeField] private string parameterName;
 
     private void Update()
     {
         foreach (ParamRef param in emitter.Params)
         {
-            if (param.Name == "RPM_Or_Sum")
+            if (param.Name == parameterName)
             {
                 param.Value = Mathf.Clamp(Mathf.Abs(carControl.ForwardSpeed()), 0, 20);
             }
