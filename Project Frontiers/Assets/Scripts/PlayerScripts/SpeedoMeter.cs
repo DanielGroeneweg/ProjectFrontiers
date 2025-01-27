d0571abc09fb28d5204d2b1bf2b3dcb82af4411f
FlipDark <sanderbenning2007@gmail.com>
2025-01-27 17:25:11 +0100

    speedometer script working

--- Project Frontiers/Assets/Scripts/PlayerScripts/SpeedoMeter.cs
+++ Project Frontiers/Assets/Scripts/PlayerScripts/SpeedoMeter.cs
@@ -1,19 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpeedoMeter : MonoBehaviour
+public class Speedometer : MonoBehaviour
 {
-    [SerializeField] private Image SpeedometerGauge;
-    
-    private bool IsSpeedUp = false;
-    private bool IsDirectionUp = true;
+    public Image fillImage; // UI Image for Fill Amount
+    public Rigidbody carRigidbody; // Reference to the car's Rigidbody
+    public float maxSpeed = 200f; // Set max speed of the car
 
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get current speed (magnitude of velocity)
+        float currentSpeed = carRigidbody.velocity.magnitude; // Convert m/s to km/h
+
+        // Normalize speed (0 to 1) and set fill amount
+        fillImage.fillAmount = Mathf.Clamp01(currentSpeed / maxSpeed);
     }
 }
