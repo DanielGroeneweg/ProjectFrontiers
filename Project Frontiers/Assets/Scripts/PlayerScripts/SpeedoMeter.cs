9d29755c2ab85d2eec9346a757cd0a3dea8f10d7
FlipDark <sanderbenning2007@gmail.com>
2025-01-27 14:14:47 +0100

    speedometer script in making experiencing with the gauge

---
+++ Project Frontiers/Assets/Scripts/PlayerScripts/SpeedoMeter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedoMeter : MonoBehaviour
+{
+    [SerializeField] private Image SpeedometerGauge;
+    
+    private bool IsSpeedUp = false;
+    private bool IsDirectionUp = true;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
