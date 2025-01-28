fb2074023c0760282d51f35293b3c1eacca8b324
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-01-28 13:09:45 +0100

    added storm progress functionality to the timer

--- Project Frontiers/Assets/Scripts/CoreScripts/PlayTimer.cs
+++ Project Frontiers/Assets/Scripts/CoreScripts/PlayTimer.cs
@@ -1,16 +1,21 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+
 [RequireComponent(typeof(TMP_Text))]
 public class PlayTimer : MonoBehaviour
 {
     // In Inspector
+    [SerializeField] private Image fillImage;
     [SerializeField] private TMP_Text timeDisplay;
     [SerializeField] private float startMinutes;
     [SerializeField] private float startSeconds;
     public UnityEvent TimeRanOut;
 
     // Internal
+    private float startTime;
+    private float time;
     private bool timerPlaying;
     private float seconds;
     private float minutes;
@@ -19,11 +24,15 @@ public class PlayTimer : MonoBehaviour
         timerPlaying = true;
         seconds = startSeconds;
         minutes = startMinutes;
+
+        time = startSeconds + startMinutes * 60;
+        startTime = time;
     }
     private void Update()
     {
         if  (timerPlaying) DoTimer();
         DisplayTimer();
+        StormProgress();
     }
     private void DoTimer()
     {
@@ -34,6 +43,7 @@ public class PlayTimer : MonoBehaviour
         }
 
         seconds -= Time.deltaTime;
+        time -= Time.deltaTime;
 
         if (minutes <= 0 && seconds <= 0) TimeRanOut?.Invoke();
     }
@@ -51,6 +61,13 @@ public class PlayTimer : MonoBehaviour
             if (seconds < 10) timeDisplay.text = ((int)minutes).ToString() + ":0" + ((int)seconds).ToString();
             else timeDisplay.text = ((int)minutes).ToString() + ":" + ((int)seconds).ToString();
         }
+    }
+    private void StormProgress()
+    {
+        float diff = 1f / startTime;
+        float secondsPlayed = startTime - time;
+        float fill = diff * secondsPlayed;
+        fillImage.fillAmount = fill;
     }
     public void StopTimer()
     {
