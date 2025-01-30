f0813bdbb66a5a5cb824f44a157b99febeb4cf9e
Daniël Groeneweg <daniel.groeneweg03@gmail.com>
2025-01-30 20:45:52 +0100

    audio mixing, fixed upgrade menu UI, fixed some scripts

--- Project Frontiers/Assets/Scripts/PlayerScripts/FollowPlayer.cs
+++ Project Frontiers/Assets/Scripts/PlayerScripts/FollowPlayer.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
+    [SerializeField] private Transform player;
+
     // Internal
     private Vector3 oldAngles;
     private void Start()
     {
-        oldAngles = transform.localEulerAngles;
+        oldAngles = player.localEulerAngles;
     }
     private void Update()
     {
-        Vector3 rotationChange = transform.localEulerAngles - oldAngles;
+        Vector3 rotationChange = player.localEulerAngles - oldAngles;
 
         transform.localEulerAngles = new Vector3(
             transform.localEulerAngles.x - rotationChange.x,
             transform.localEulerAngles.y,
             transform.localEulerAngles.z - rotationChange.z);
 
-        oldAngles = transform.localEulerAngles;
+        oldAngles = player.localEulerAngles;
     }
 }
