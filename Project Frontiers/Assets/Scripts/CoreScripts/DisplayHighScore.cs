822bb3f0224c6200fd45f46bba9d62a5b5c1d171
Haelinos <hznieland@outlook.com>
2025-01-30 14:38:34 +0100

    iets

--- Project Frontiers/Assets/Scripts/CoreScripts/DisplayHighScore.cs
+++ Project Frontiers/Assets/Scripts/CoreScripts/DisplayHighScore.cs
@@ -6,6 +6,7 @@ public class DisplayHighScore : MonoBehaviour
     [SerializeField] private TMP_Text highscoreDisplay;
     private void Start()
     {
-        highscoreDisplay.text = "Highscore: " + PlayerPrefs.GetInt("HighScore");
+        float highscore = Mathf.Floor((PlayerPrefs.GetInt("HighScore") / 1000f) * 10.0f) * 0.1f;
+        highscoreDisplay.text = highscore.ToString() + "K";
     }
 }
