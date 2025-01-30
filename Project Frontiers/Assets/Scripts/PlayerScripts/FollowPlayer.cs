81b1cc43d40e369ae825ba8dc2925345e7c937c2
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-01-30 14:37:05 +0100

    fixed camera rotation (not tested yet)

--- Project Frontiers/Assets/Scripts/PlayerScripts/FollowPlayer.cs
+++ Project Frontiers/Assets/Scripts/PlayerScripts/FollowPlayer.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
+    [SerializeField] private Transform player;
     // Internal
     private Vector3 oldAngles;
     private void Start()
     {
-        oldAngles = transform.localEulerAngles;
+        oldAngles = player.localEulerAngles;
     }
     private void Update()
     {
-        Vector3 rotationChange = transform.localEulerAngles - oldAngles;
+        Vector3 rotationChange = player.localEulerAngles - oldAngles;
 
         transform.localEulerAngles = new Vector3(
-            transform.localEulerAngles.x - rotationChange.x,
-            transform.localEulerAngles.y,
-            transform.localEulerAngles.z - rotationChange.z);
+            player.localEulerAngles.x - rotationChange.x,
+            player.localEulerAngles.y,
+            player.localEulerAngles.z - rotationChange.z);
 
-        oldAngles = transform.localEulerAngles;
+        oldAngles = player.localEulerAngles;
     }
 }
