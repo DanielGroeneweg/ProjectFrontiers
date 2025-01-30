3a45b8d65a8100cd81990a45d3fa3b53d3bdaf51
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-01-30 12:30:22 +0100

    edited highscore display to show it in thousands rather than the entire score, added button click sound to fmod project

--- Project Frontiers/Assets/Scripts/CoreScripts/DisplayHighScore.cs
+++ Project Frontiers/Assets/Scripts/CoreScripts/DisplayHighScore.cs
@@ -6,6 +6,7 @@ public class DisplayHighScore : MonoBehaviour
     [SerializeField] private TMP_Text highscoreDisplay;
     private void Start()
     {
-        highscoreDisplay.text = PlayerPrefs.GetInt("HighScore").ToString();
+        float highscore = Mathf.Floor((PlayerPrefs.GetInt("HighScore") / 1000f) * 10.0f) * 0.1f;
+        highscoreDisplay.text = highscore.ToString() + "K";
     }
 }
