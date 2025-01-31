39f4c7fd3acfa634f560eb75bedc0690ed83683c
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-01-31 09:37:18 +0100

    made the booster slightly less sudden

--- Project Frontiers/Assets/Scripts/CoreScripts/RampSpeedBooster.cs
+++ Project Frontiers/Assets/Scripts/CoreScripts/RampSpeedBooster.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class RampSpeedBooster : MonoBehaviour
 {
     [SerializeField] private float speedBoostAmount;
-    private void OnTriggerEnter(Collider collision)
+    private void OnTriggerStay(Collider collision)
     {
         if (collision.tag == "Player")
         {
             Rigidbody rb = collision.transform.root.gameObject.GetComponent<Rigidbody>();
             CarControl carControl = collision.transform.root.gameObject.GetComponent<CarControl>();
 
-            rb.AddForce(collision.transform.forward * speedBoostAmount);
+            rb.AddForce(collision.transform.forward * speedBoostAmount * Time.deltaTime);
         }
     }
 }
